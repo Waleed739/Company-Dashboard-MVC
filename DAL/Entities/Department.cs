52aac64c968760d94aa1fbd82c5acf9281d60cea
Waleed739 <82949827+Waleed739@users.noreply.github.com>
2023-12-26 18:07:36 +0200

    Add files via upload

---
+++ DAL/Entities/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Department:BaseEntity
+    {
+        public int Id { get; set; }
+        public string Phone { get; set; }
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; } = DateTime.Now;
+    }
+}
